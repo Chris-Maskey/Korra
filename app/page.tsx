63dace83e4680591b077370a86e32e9aea494aeb
Chris-Maskey <chrismaskey03@gmail.com>
2024-12-14 08:02:17 +0000

    Add supabase ssr auth and middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="h-full ">
+      <div className="flex w-full items-center justify-end">
+        <ModeToggle />
+      </div>
+      <h1 className="text-7xl font-bold tracking-tight">Korra</h1>
+    </main>
+  );
 }
